506af3a8cf682cdb48a6a515abb1507676d1788d
Rig <rigels1304@gmail.com>
2024-01-04 22:23:59 +0100

    Added scss navbar

--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import { Link } from "react-router-dom";
-import "./Navbar.css";
+import "./Navbar.scss";
 import Notifications from "../../pages/Notifications/Notifications";
 import Messages from "../../pages/Messages/Messages";
 const Navbar: React.FC = () => {
