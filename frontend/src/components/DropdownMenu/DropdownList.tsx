cef7c1de675eae8ac53e5d7cd237e7a02933eec4
Michael Deng <39012169+Mzon2857@users.noreply.github.com>
2024-01-17 18:42:08 -0500

    navbar profile icon fix

--- frontend/src/components/DropdownMenu/DropdownList.tsx
+++ frontend/src/components/DropdownMenu/DropdownList.tsx
@@ -1,11 +1,11 @@
 import { Link } from "react-router-dom";
 import messagesimg from "../../assets/envelope.png";
 import bellimg from "../../assets/bell.png";
+import dropDownIcon from "../../assets/downArrow.png";
 import { useAuth0 } from "@auth0/auth0-react";
-import "./DropdownList.scss"
+import "./DropdownList.scss";
 import DropdownItem from "./DropdownItem";
-import DropdownMenu from "./DropdownMenu"; 
-
+import DropdownMenu from "./DropdownMenu";
 
 function DropdownList() {
   const { user } = useAuth0();
@@ -14,22 +14,25 @@ function DropdownList() {
     <ul className="rightSide">
       <li>
         <Link to="/messages">
-          <DropdownItem icon={messagesimg}/>
+          <DropdownItem icon={messagesimg} />
         </Link>
       </li>
       <li>
         <Link to="/notifications">
-        <DropdownItem icon={bellimg}/>
+          <DropdownItem icon={bellimg} />
         </Link>
       </li>
       <li>
         <Link to={`/${user?.nickname}`}>
+          <img src={user?.picture} alt={user?.name} className="user-profile-image" />
+        </Link>
+      </li>
+      <li>
         <div className="special-icon">
-          <DropdownItem icon={user?.picture}>
-            <DropdownMenu/>
+          <DropdownItem icon={dropDownIcon}>
+            <DropdownMenu />
           </DropdownItem>
         </div>
-        </Link>
       </li>
     </ul>
   );
